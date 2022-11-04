cb10cc1237aeed4795ad401ed1dc5564219e0fff
preshpi <egwuenuprecious7@gamil.com>
2022-11-04 22:13:49 +0100

    Bug fix: fixed input/submit

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -7,6 +7,22 @@ function Contact() {
   const [message, setMessage] = useState('');
   const [check, setCheck] = useState('');
   const [error, setError] = useState(false);
+  const [emailMessage, setEmailmessage] = useState('');
+
+  const handleOnChange=(e)=> {
+    setEmail(e.target.value);
+  }
+
+  const emailValidation=()=> {
+    const regEx = /[a-zA-Z0-9,_%+-]+@[a-z0-9*-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
+    if(regEx.test(email)){
+      setEmailmessage("")
+    }else if(!regEx.test(email) && email !== '') {
+      setEmailmessage("Email is not valid!");
+    }else{
+      setEmailmessage("");
+    }
+  }
 
   const handleSubmit=(e) => {
     e.preventDefault();
@@ -14,10 +30,11 @@ function Contact() {
       setError(true)
     }
     if(firstName&&lastName&&email&&message&&check){
-      console.log("first Name:",firstName,"\nLast Nmae:", lastName, "\nEmail:", email,"\nMessage:", message, check); 
-    }
-   
+      console.log("first Name:",firstName,"\nLast Nmae:", lastName, "\nEmail:", email,"\nMessage:", message,"\nCheckbox:", check); 
+    }   
   }
+
+
   return (
     <div className="grid lg:place-items-center  h-screen items-center justify-center">
       <div className="m-5">
@@ -29,38 +46,40 @@ function Contact() {
           <div className="grid lg:grid-cols-2 lg:gap-5">
             <div>
               <label for="first_name" className="name">First name</label>
-              <input className={error&& firstName.length<=0 ? 'inputname mt-3 w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]' : 'inputname mt-3 w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#F83F23] border border-[#D0D5DD]' } />
-            {/* {error&& firstName.length<=0 ?  <input onChange={e=>setFirstname(e.target.value)} className="inputname mt-3 w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]" id="first_name" placeholder='Enter your first name'/> : "focus:border-[#F83F23]"} */}
-             {error&& firstName.length<=0 ? <p class="hint2 mt-2">Please enter your firstname</p> : ""}
+              <input onChange={e=>setFirstname(e.target.value)}  className={error&& firstName.length<=0 ? 'errorname mt-3 w-[343px] lg:w-[348px] focus:outline-none border border-[#F83F23]' : 'inputname mt-3  w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]' } id="first_name" placeholder='Enter your last name'/>
+              {error&& firstName.length<=0 ? <p class="hint2 mt-2">Please enter your firstname</p> : ""}
             </div>
 
             <div>
               <label for="last_name" className="name">Last name</label>
-              <input onChange={e=>setLastname(e.target.value)}  className="inputname mt-3  w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]" id="last_name" placeholder='Enter your last name'/>
+              <input onChange={e=>setLastname(e.target.value)}  className={error&& lastName.length<=0 ? 'errorname mt-3 w-[343px] lg:w-[348px] focus:outline-none border border-[#F83F23]' : 'inputname mt-3  w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]' } id="last_name" placeholder='Enter your last name'/>
               {error&& lastName.length<=0 ? <p class="hint2 mt-2">Please enter your lastname</p> : ""}
             </div>
           </div>
 
           <div className="mt-3">
             <label className="name" for="email">Email</label>
-            <input onChange={e=>setEmail(e.target.value)}  className="emailinput  w-[343px] lg:w-[720px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]" id="email" placeholder="yourname@email.com"/>
+            <input onChange={e=>setEmail(e.target.value)}  className={error&& email.length<=0 ? 'errorname mt-3 w-[343px] lg:w-[720px] focus:outline-none border border-[#F83F23]' : 'inputname mt-3  w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]' } id="email" placeholder="yourname@email.com" value={email} onChange={handleOnChange}/>
+            <p className="hint2 mt-2">{emailMessage}</p>
             {error&& email.length<=0 ? <p class="hint2 mt-2">Please enter your email</p> : ""}
           </div>
 
           <div className="mt-3">
             <label className="name" for="message">Message</label>
-            <textarea onChange={e=>setMessage(e.target.value)}  className="messageinput mt-3 w-[343px] lg:w-[720px] focus:outline-none focus:ring focus:border-[#84CAFF]  border border-[#D0D5DD]" id="message" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+            <textarea onChange={e=>setMessage(e.target.value)}  className={error&& message.length<=0 ? 'errormessage mt-3 w-[343px] lg:w-[720px] focus:outline-none border border-[#F83F23]' : 'messageinput mt-3  w-[343px] lg:w-[348px] focus:outline-none focus:border focus:border-[#84CAFF]  border border-[#D0D5DD]' } id="message" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
             {error&& message.length<=0 ?  <p class="hint2 mt-2">Please enter a message</p> : ""}
           </div>
 
           <div className="mt-3">
-            <input onChange={e=>setCheck(e.target.value)} type="checkbox" id="text" className="checkbox mx-2 ring-opacity-25  focus:ring-2 ring-[#84CAFF] focus:border-[#84CAFF]   border border-[#D0D5DD] focus:outline-none"/>
-            <label  for="text lg:w-[688px] w-[311px] mb-5">You agree to providing your data to "name" who may contact you.</label>
-            {error&& check.length<=0 ?  <p class="hint2 mt-2">check the box</p> : ""}
+            <div className="flex">
+            <input onChange={e=>setCheck(e.target.value)} type="checkbox" id="text" className="checkbox mx-2 mb-3 ring-opacity-25  focus:ring-2 ring-[#84CAFF] focus:border-[#84CAFF]   border border-[#D0D5DD] focus:outline-none"/>
+            <label  for="text lg:w-[688px] w-[311px]">You agree to providing your data to Preshypie who may contact you.</label>
+            </div>       
+            {error&& check.length<=0 ?  <p class="hint2 mt-2 mx-2">check the box</p> : ""}
           </div>
 
           <div>
-            <button id="btn__submit" className="mt-3 w-[343px] lg:w-[720px] bg-[#1570EF] hover:bg-[#B2DDFF] ring-opacity-25  focus:ring-4 ring-[#84CAFF]">Send message</button>
+            <button id="btn__submit" onClick={emailValidation} className="mt-3 w-[343px] lg:w-[720px] bg-[#1570EF] hover:bg-[#B2DDFF] ring-opacity-25  focus:ring-4 ring-[#84CAFF]">Send message</button>
           </div>
 
           </form>
